98af91b131e92d7be5712058665492838f7e33d4
shubham.kumar <shubham.kumar@marblex.ai>
2025-02-18 16:15:49 +0530

    quick ui

---
+++ frontend/src/pages/ManuallForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LoginForm } from '../components/ui/login-form'
+
+const ManuallForm = () => {
+  return (
+    <div className='py-2'>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default ManuallForm
