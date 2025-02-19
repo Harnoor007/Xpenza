fc801538a0f9ef4d8b23506d5b1c995d8d531d19
shubham.kumar <shubham.kumar@marblex.ai>
2025-02-19 16:27:14 +0530

    Initial Commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,10 @@ function App() {
   return (
 
     <>
-    <div className='h-screen bg-gray-200 py-4 px-4'>
-      <div className='upper py-2 flex flex-col items-center '>
+    <div className='h-[200vh] bg-gray-200 py-4 px-4'>
+      <div className='upper py-2 flex flex-col items-center gap-4'>
         <h1 className='text-5xl p-4 rounded-lg bg-gradient-to-r from-orange-200 to-blue-300 tracking-wider font-semibold '>Xpenza <span className='text-lg text-orange-900'>Expense Tracker App</span></h1>
-        <img src="./hero image.jpeg" alt="" className='w-4/5 rounded-xl object-cover '/>
+        <img src="./hero image.jpeg" alt="" className='w-1/2 md:w-2/5  lg:w-2/5 rounded-xl object-cover '/>
       </div>
       <div className='lower mt-12 flex justify-center pb-16 gap-2'>
           <Button size={'lg'} variant={isUploadTab ? 'default' : 'secondary'}  onClick={() => setIsUploadTab(true)}>Upload Your Bill </Button>
@@ -20,11 +20,20 @@ function App() {
       {
         isUploadTab ? (
           <>
-          <IsUpload />
+           <div className="flex justify-center ">
+            <div className="w-1/2">
+              <IsUpload />
+            </div>
+
+          </div>
           </>
         ) : (
           <>
+        <div className="flex justify-center ">
+        <div className="w-1/2">
           <ManuallForm />
+          </div>
+          </div>
           </>
         )
       }
